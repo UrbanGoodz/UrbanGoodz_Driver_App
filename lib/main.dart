506a02b292033e9c7a700333adef31ff09fee7e8
UrbanGoodz <dagoodsbe2@gmail.com>
2026-07-10 17:25:27 -0500

    Initial commit: Urban Goodz Driver App baseline for tester release

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:urban_goodz_driver/controllers/driver_auth_controller.dart';
+import 'package:urban_goodz_driver/services/api_client.dart';
+import 'package:urban_goodz_driver/services/driver_api_service.dart';
+import 'package:urban_goodz_driver/screens/dashboard_screen.dart';
+import 'package:urban_goodz_driver/screens/driver_onboarding_screen.dart';
+import 'package:urban_goodz_driver/theme/app_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final authController = Get.put(DriverAuthController());
+    Get.put(ApiClient());
+    Get.put(DriverApiService());
+    return Obx(() {
+      return GetMaterialApp(
+        title: 'Urban Goodz Driver',
+        theme: AppTheme.lightTheme,
+        debugShowCheckedModeBanner: false,
+        home: authController.isLoggedIn.value
+            ? const DashboardScreen()
+            : const DriverOnboardingScreen(),
+      );
+    });
+  }
+}
