5360e73c3aeaa6ce8be75d93c646f79c0c3c1e62
UrbanGoodz <dagoodsbe2@gmail.com>
2026-07-16 09:00:01 -0500

    Phase 10: Fix driver app package name and remove mock data

--- driver_app/android/app/build.gradle.kts
+++ driver_app/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.urbaneatz.driver"
+    namespace = "com.urbangoodz.driver"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.urbaneatz.driver"
+        applicationId = "com.urbangoodz.driver"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
